04658e29dcec6cdf7fb33b5048f227c345092044
Roan <34751522+RoanM1@users.noreply.github.com>
2018-02-12 13:11:34 -0800

    dsaf

---
+++ keemstar/keem.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell") 
+WshShell.Run chr(34) & "keem/batch/keem.bat" & Chr(34), 0
+Set WshShell = Nothing
--- keemstar/keem/batch/keem.bat
+++ keemstar/keem/batch/keem.bat
@@ -1,5 +1,11 @@
 @echo off
 lol
+@echo off
+if not defined FOO (
+  set FOO=1
+  start /min "" %~0
+  exit /b
+)
 start http://cdn.roanm.xyz/beach/
 ping 127.0.0.1 >nul
 start http://cdn.roanm.xyz/ice.gif
